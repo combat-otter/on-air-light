0b09b09c8feafa49ff109dccfbdcdb7a5f7e1827
combat-otter <combat@otter.com>
2023-08-24 11:29:32 +0100

    add some hysteresis

--- windows_mic_monitor.py
+++ windows_mic_monitor.py
@@ -17,7 +17,6 @@ Requires a local config.json file with the following content:
 }
 """
 
-
 # console logging setup
 logging.basicConfig(format="%(asctime)s %(levelname)-8s %(message)s", level=logging.INFO, datefmt="%Y-%m-%d %H:%M:%S")
 
@@ -26,8 +25,8 @@ with open("config.json") as config_file:
     TAPO_CREDS = json.load(config_file)
 
 
-def is_microphone_active():
-    active = False
+def get_app_using_mic():
+    app_name = None
 
     with winreg.OpenKey(
         winreg.HKEY_CURRENT_USER,
@@ -43,16 +42,14 @@ def is_microphone_active():
 
                     if last_used_time_stop == 0:
                         app_name = re.sub(r"_([a-z0-9])+$", "", sub_key_name)
-                        logging.info(f"{app_name} is using the microphone")
-                        active = True
                         break
 
                 except FileNotFoundError:
                     # reg values not found, skip
                     pass
 
-    if active:
-        return active
+    if app_name:
+        return app_name
 
     with winreg.OpenKey(
         winreg.HKEY_CURRENT_USER,
@@ -69,36 +66,58 @@ def is_microphone_active():
 
                     if last_used_time_stop == 0:
                         app_name = re.sub(r"([\w:# \(\)\-])+#", "", sub_key_name)
-                        logging.info(f"{app_name} is using the microphone")
-                        active = True
                         break
 
                 except FileNotFoundError:
                     # reg values not found, skip
                     pass
 
-    return active
+    return app_name
 
 
 def main():
-    response = tapoPlugApi.getDeviceInfo(TAPO_CREDS)
-    print(f"Using Tapo plug: {response}")
-    plug_on = False
+    response = json.loads(tapoPlugApi.getDeviceInfo(TAPO_CREDS))
+    pretty = json.dumps(response, indent=4, sort_keys=True)
+    logging.info(f"Using Tapo plug: {pretty}")
 
-    while True:
-        if is_microphone_active():
-            if not plug_on:
+    # set correct initial state
+    switched_on = response["result"]["device_on"]
+
+    active_seconds = 0
+    inactive_seconds = 0
+    hysteresis = 10
+
+    try:
+        while True:
+            if int(time.time()) % 10 == 0:
+                response = json.loads(tapoPlugApi.getDeviceInfo(TAPO_CREDS))
+                logging.info(f"Plug powered: {response['result']['device_on']}")
+
+            app_using_mic = get_app_using_mic()
+            if app_using_mic:
+                if active_seconds <= hysteresis:
+                    logging.info(f"{app_using_mic} is using the microphone")
+
+                active_seconds += 1
+                inactive_seconds = 0
+            else:
+                active_seconds = 0
+                inactive_seconds += 1
+
+            if active_seconds > hysteresis and not switched_on:
                 logging.info("Switching plug on")
                 response = json.loads(tapoPlugApi.plugOn(TAPO_CREDS))
-                plug_on = response["error_code"] == 0
+                switched_on = response["error_code"] == 0
 
-        else:
-            if plug_on:
+            elif inactive_seconds > hysteresis and switched_on:
                 logging.info("Switching plug off")
                 response = json.loads(tapoPlugApi.plugOff(TAPO_CREDS))
-                plug_on = not response["error_code"] == 0
+                switched_on = not response["error_code"] == 0
 
-        time.sleep(5)
+            time.sleep(1)
+
+    except KeyboardInterrupt:
+        pass
 
 
 if __name__ == "__main__":
